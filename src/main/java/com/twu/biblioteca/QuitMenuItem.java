2db1a5a142c7204beb3dd283c73aea25c4d631ed
Puja Gupta <pujag@thoughtworks.com>
2017-06-17 16:58:00 +0530

    Implement quit menu

---
+++ src/main/java/com/twu/biblioteca/QuitMenuItem.java
@@ -0,0 +1,27 @@
+package com.twu.biblioteca;
+
+import com.twu.inputOutput.OutputWriter;
+
+/**
+ * Menu item for quitting from the application
+ */
+public class QuitMenuItem implements MenuItem {
+
+    OutputWriter outputWriter;
+    public static final String QUIT_MESSAGE = "Thanks for using Biblioteca\n";
+    public static final String MENU_NAME = "Quit Biblioteca\n";
+
+    QuitMenuItem(OutputWriter outputWriter) {
+        this.outputWriter = outputWriter;
+    }
+
+    @Override
+    public void performAction() {
+        outputWriter.write(QUIT_MESSAGE);
+    }
+
+    @Override
+    public String getMenuName() {
+        return MENU_NAME;
+    }
+}
---
+++ src/test/java/com/twu/biblioteca/QuitMenuItemTest.java
@@ -0,0 +1,22 @@
+package com.twu.biblioteca;
+
+import com.twu.mockModels.TestOutputWriter;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test for Quit menu
+ */
+public class QuitMenuItemTest {
+
+    @Test
+    public void shouldWriteQuitMessageWhenCalled() {
+        TestOutputWriter testOutputWriter = new TestOutputWriter();
+        QuitMenuItem quitMenuItem = new QuitMenuItem(testOutputWriter);
+
+        quitMenuItem.performAction();
+
+        assertEquals(QuitMenuItem.QUIT_MESSAGE, testOutputWriter.getOutput().get(0));
+    }
+}
